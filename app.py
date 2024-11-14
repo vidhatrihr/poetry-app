be439dc0a74fafdc1087df9a80801b35e8d652be
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2024-11-14 21:26:15 +0530

    added search feature

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from functools import lru_cache
 import requests
 
 app = Flask(__name__)
 
 
+@lru_cache
+def get_json(url):
+  return requests.get(url).json()
+
+
 @app.route('/')
 def index():
-  data = requests.get('https://poetrydb.org/author').json()
+  data = get_json('https://poetrydb.org/author')
   return render_template('index.html', data=data)
+
+
+@app.route('/search')
+def search():
+  """ http://localhost:5000/search?title=... """
+  title = request.args.get('title')
+  data = get_json(f'https://poetrydb.org/title/{title}/author,title')
+  return render_template('search.html', title=title, data=data)
 
 
 @app.route('/titles/<author>')
 def titles(author):
-  data = requests.get(f'https://poetrydb.org/author/{author}/title').json()
+  data = get_json(f'https://poetrydb.org/author/{author}/title')
   return render_template('titles.html', author=author, data=data)
 
 
 @app.route('/read/<author>/<title>')
 def read(author, title):
-  data = requests.get(f'https://poetrydb.org/author,title/{author}:abs;{title}:abs').json()
+  data = get_json(f'https://poetrydb.org/author,title/{author}:abs;{title}:abs')
   return render_template('read.html', author=author, title=title, poetry=data[0])
 
 
